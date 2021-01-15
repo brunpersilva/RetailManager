c8f27948616f0141d996c971f3948e76e136ddd7
Bruno Silva <brunpersilva@gmail.com>
2021-01-15 13:04:17 -0300

    Register DB tables
    Tables for basic register system

---
+++ RMData/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] nvarchar(128) not null PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [EmailAdress] NVARCHAR(256) NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(),
+)
