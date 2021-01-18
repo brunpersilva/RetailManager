ce6bf72b0adedffaaa2b1265fd2a5846a8e04087
Bruno Silva <brunpersilva@gmail.com>
2021-01-18 19:36:49 -0300

    Added GetUserById Method
    Added UserController that gets user
    information based on who is logged in.

--- RMDataManager.Library/DataAcess/UserData.cs
+++ RMDataManager.Library/DataAcess/UserData.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RMDataManager.Library.Models;
+using RMDataManager.Library.Internal.DataAcess;
 
 namespace RMDataManager.Library.DataAcess
 {
-    class UserData
+    public class UserData
     {
+        public List<UserModel> GetUSerById(string Id)
+        {
+            SqlDataAcess sql = new SqlDataAcess();
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "RMData");
+
+            return output;
+        }
     }
 }
---
+++ RMDataManager/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using RMDataManager.Library.DataAcess;
+using RMDataManager.Library.Models;
+
+namespace RMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            UserData data = new UserData();
+
+            return data.GetUSerById(userId);
+        }
+    }
+}
