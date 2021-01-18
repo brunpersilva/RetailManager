34e188b3e7e5dc9dbefbf1c080bfc8cf3b81a360
Bruno Silva <brunpersilva@gmail.com>
2021-01-17 23:29:38 -0300

    Created DataManager Project
    Added Classes that save and get data from SQL
    using Dapper

---
+++ RMDataManager.Library/Internal/DataAcess/SqlDataAcess.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace RMDataManager.Library.Internal.DataAcess
+{
+    internal class SqlDataAcess
+    {
+        public string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public List<T> LoadData<T, U>(string storedProcedure, U paramater, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(storedProcedure, paramater,
+                    commandType: CommandType.StoredProcedure).ToList();
+
+                return rows;
+            }
+        }
+        public void SaveData<T>(string storedProcedure, T paramater, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(storedProcedure, paramater,
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+
+    }
+}
