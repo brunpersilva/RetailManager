7967197f990115fb6a3b0987aeddee8f0788d5eb
Bruno Silva <brunpersilva@gmail.com>
2021-05-19 23:43:47 -0300

    Added register page to add users.

--- Portal/Program.cs
+++ Portal/Program.cs
@@ -23,14 +23,16 @@ namespace Portal
             builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddAuthorizationCore();
             builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+           
             
             builder.Services.AddSingleton<IAPIHelper, APIHelper>();
             builder.Services.AddSingleton<ILoggedInUserModel, LoggedInUserModel>();
             builder.Services.AddTransient<IProductEndpoint, ProductEndpoint>();
             builder.Services.AddTransient<ISaleEndpoint, SaleEndpoint>();
-            
+            builder.Services.AddTransient<IUserEndpoint, UserEndpoint>();
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
             await builder.Build().RunAsync();
         }
     }
--- RMDesktopUI.Library/Models/CreateUserModel.cs
+++ RMDesktopUI.Library/Models/CreateUserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
@@ -8,9 +9,11 @@ namespace RMDesktopUI.Library.Models
     public class CreateUserModel
     {
         [Required]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
 
         [Required]
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
 
         [Required]
@@ -21,6 +24,7 @@ namespace RMDesktopUI.Library.Models
         public string Password { get; set; }
 
         [Required]
+        [DisplayName("Confirm Password")]
         [Compare(nameof(Password), ErrorMessage ="The passwords do not match.")]
         public string ConfirmPassword { get; set; }
     }
